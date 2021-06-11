6d8a52349986dc38fe0a839dd37f865b97915273
Vitor Moschetti <vitormoschetti@terra.com.br>
2021-06-11 16:59:16 -0300

    project

---
+++ src/test/java/br/com/DynamoDB/DynamoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.DynamoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

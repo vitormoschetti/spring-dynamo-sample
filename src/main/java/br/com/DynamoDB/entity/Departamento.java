b99797bce933928b2c686256ca5322e104f31feb
Vitor Moschetti <vitormoschetti@terra.com.br>
2021-06-11 17:16:42 -0300

    criando entidades

---
+++ src/main/java/br/com/DynamoDB/entity/Departamento.java
@@ -0,0 +1,21 @@
+package br.com.DynamoDB.entity;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@DynamoDBDocument
+@NoArgsConstructor
+@AllArgsConstructor
+public class Departamento {
+
+    @DynamoDBAttribute
+    private String nome;
+
+    @DynamoDBAttribute
+    private String codigo;
+
+}

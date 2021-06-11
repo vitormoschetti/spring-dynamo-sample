0195682293d6313d752954db9d5c6ee48503a550
Vitor Moschetti <vitormoschetti@terra.com.br>
2021-06-11 17:22:21 -0300

    dynamo config

---
+++ src/main/java/br/com/DynamoDB/config/DynamoDbConfiguration.java
@@ -0,0 +1,44 @@
+package br.com.DynamoDB.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDbConfiguration {
+
+    @Bean
+    public DynamoDBMapper dynamoDBMapper() {
+        return new DynamoDBMapper(buildAmazonDynamoDB());
+    }
+
+    private AmazonDynamoDB buildAmazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(endpointConfiguration())
+                .withCredentials(credentialsProvider())
+                .build();
+    }
+
+    private AwsClientBuilder.EndpointConfiguration endpointConfiguration() {
+        return new AwsClientBuilder.EndpointConfiguration(
+                "http://localhost:8000/",
+                "sa-east-1"
+        );
+    }
+
+    private AWSStaticCredentialsProvider credentialsProvider() {
+        return new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials(
+                        "fakeId",
+                        "fakeSecret"
+                )
+        );
+    }
+
+}
